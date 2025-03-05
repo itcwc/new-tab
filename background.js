934e200ea7bff4eae847f571b5b967b69da5c4dd
it_cwc <1766987395@qq.com>
2025-03-06 00:22:34 +0800

    feat(newtab): 新增新标签页重定向功能
    - 添加新标签页重定向逻辑，当用户打开 newtab 时重定向到自定义页面
    - 更新 newtab.css 样式，优化搜索框和按钮的视觉效果
    - 在 manifest.json 中添加 declarativeNetRequest 权限，用于未来可能的扩展功能

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
         const imageInfo = data.images[0];
         imageInfo.url = 'https://cn.bing.com' + data.images[0].url;
-        
+
         sendResponse({ imageInfo });
       })
       .catch(error => {
@@ -19,5 +19,54 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         sendResponse({ error: '背景图片加载失败' });
       });
     return true;  // 需要异步响应
+  }
+});
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === "toggleNewTab") {
+//       chrome.storage.sync.get("enableNewTab", function (data) {
+//           if (data.enableNewTab) {
+//               chrome.declarativeNetRequest.updateDynamicRules({
+//                   removeRuleIds: [1],
+//                   addRules: [{
+//                       id: 1,
+//                       priority: 1,
+//                       action: { type: "redirect", redirect: { url: chrome.runtime.getURL("newtab.html") }},
+//                       condition: { urlFilter: "chrome://newtab/", resourceTypes: ["main_frame"] }
+//                   }]
+//               });
+//           } else {
+//               chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [1] });
+//           }
+//       });
+//   }
+// });
+
+// chrome.tabs.onCreated.addListener((tab) => {
+
+//   console.log('tabs.onCreated');
+
+//   chrome.storage.sync.get("enableNewTab", function (data) {
+
+//       console.log(data.enableNewTab);
+
+//       if (data.enableNewTab && tab.url === "chrome://newtab/") {
+//           chrome.tabs.update(tab.id, { url: chrome.runtime.getURL("newtab.html") });
+//       }
+//   });
+// });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "loading" && (tab.url === "chrome://newtab/" || tab.url === "about:blank")) {
+    chrome.storage.sync.get("enableNewTab", function (data) {
+      console.log("新标签页检测：", data.enableNewTab, tab.url);
+      if (data.enableNewTab) {
+        // 先创建新的标签页
+        chrome.tabs.create({ url: chrome.runtime.getURL("newtab.html") }, () => {
+          // 关闭原来的 new tab
+          chrome.tabs.remove(tabId);
+        });
+      }
+    });
   }
 });
